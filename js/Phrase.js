68e58575f6d9acbefb2ffcc7bb16f0b468f794ca
Manny <manny@holdex.io>
2020-12-07 12:55:41 +0800

    Update Phrase.js
    - created Phrase class
    - declared Phrase constructor
    - added addPhraseToDisplay() method to Phrase class

--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,33 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+
+ class Phrase {
+     constructor(phrase) {
+        this.phrase = phrase.toLowerCase();
+     }
+
+    /**
+    * Display phrase on game board
+    */
+    addPhraseToDisplay() {
+        // select phrase div
+        const phraseDiv = document.getElementById('phrase');
+        // break this.phrase into an array of its characters including letters and spaces
+        const characters = this.phrase.split('')
+        // create li element for each character according to example_phrase_html.txt template
+        characters.forEach(character => {
+            // if character is letter, use 'letter' class and appropriate letter class
+            // if character is space, use 'space' class
+            let li;
+            if (character !== ' ') {
+                li = `<li class="hide letter ${character}">${character}</li>`;
+            } else {
+                li = `<li class="space"> </li>`;
+            }
+            // add/append li elements to board
+            phraseDiv.firstElementChild.innerHTML += li;
+        });
+        
+    }
+ }
