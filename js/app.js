98fec8675635bcb65bad0dd5af8353af19331171
Manny <manny@holdex.io>
2020-12-07 12:57:20 +0800

    Update app.js
    Added game initialization logic

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+
+// const game = new Game();
+// game.startGame();
+// console.log(`Active Phrase - phrase: ${game.activePhrase.phrase}`);
+let game;
+
+document.getElementById('btn__reset').addEventListener('click', () => {
+    game = new Game();
+    game.startGame();
+});
+
