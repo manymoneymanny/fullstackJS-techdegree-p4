437018b391ac4c0291c87e851ce648d98f013b20
Manny <manny@holdex.io>
2020-12-07 23:42:32 +0800

    Update Game.js
    handleInteraction() and helper methods:
     - checkForWin()
     - removeLife()
     - gameOver()
    plus gameboard reset actions

--- js/Game.js
+++ js/Game.js
@@ -40,4 +40,83 @@
         this.activePhrase = this.getRandomPhrase()
         this.activePhrase.addPhraseToDisplay();
     }
+
+    /**
+    * Handles onscreen keyboard button clicks
+    * @param {HTMLButtonElement} button - The clicked button element
+    */
+    handleInteraction(button) {
+        // - The clicked/chosen letter must be captured.
+        const letter = button.innerHTML;
+        // - The clicked letter must be checked against the phrase for a match.
+        if ( this.activePhrase.checkLetter(letter) ) {
+            // - If there’s a match, the letter must be displayed on screen instead of the placeholder.
+            button.classList.add('chosen');
+            button.disabled = true;
+            this.activePhrase.showMatchedLetter(letter);
+            if ( this.checkForWin() ) {
+                this.gameOver(true);
+            }
+        } else {
+            // - If there’s no match, the game must remove a life from the scoreboard.
+            button.classList.add('wrong');
+            button.disabled = true;
+            this.removeLife();
+        }
+        // - The game should check if the player has won the game by revealing all of the letters in the phrase or if the game is lost because the player is out of lives.
+        // - If the game is won or lost, a message should be displayed on screen.
+    }
+
+    /**
+    * Checks for winning move
+    * @return {boolean} True if game has been won, false if game wasn't won
+    */
+    checkForWin() {
+        return document.querySelectorAll('.hide').length === 0? true: false;
+    }
+
+    /**
+    * Increases the value of the missed property
+    * Removes a life from the scoreboard
+    * Checks if player has remaining lives and ends game if player is out
+    */
+    removeLife() {
+        this.missed++;
+        const tries = document.querySelectorAll('.tries');
+        const i = 5 - this.missed;
+        tries[i].firstElementChild.src = 'images/lostHeart.png';
+        if(this.missed === 5){
+            this.gameOver(false);
+        }
+    }
+
+    /**
+    * Displays game over message
+    * Resets the gameboard in preparation for another game
+    * @param {boolean} gameWon - Whether or not the user won the game
+    */
+    gameOver(gameWon) {
+        const overlay = document.getElementById('overlay');
+        const gameOverMessage = document.getElementById('game-over-message');
+        overlay.style.display = 'inherit';
+        if(gameWon) {
+            gameOverMessage.innerHTML = "You got this! Phrase hunt successful!";
+            overlay.classList.replace('start', 'win');
+        } else {
+            gameOverMessage.innerHTML = "Oh, no! You're out of lives. Better luck next time!";
+            overlay.classList.replace('start', 'lose');
+        }
+        const phraseList = document.getElementById('phrase').firstElementChild;
+        while ( phraseList.hasChildNodes() ) {
+            phraseList.removeChild(phraseList.firstChild);
+        }
+        document.querySelectorAll('.key').forEach(element => {
+            element.disabled = false;
+            element.classList.remove('chosen', 'wrong');
+            element.classList.add('key');
+        });
+        document.querySelectorAll('.tries').forEach(element => {
+            element.firstElementChild.src = 'images/liveHeart.png';
+        });
+    }
  }
