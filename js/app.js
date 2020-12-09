ef040e7584941641a07304792df0e4e4ba587448
Manny <manny@holdex.io>
2020-12-09 15:15:20 +0800

    Update app.js
    'Exceeds' event listener for user keyboard use in-game

--- js/app.js
+++ js/app.js
@@ -2,18 +2,34 @@
  * Project 4 - OOP Game App
  * app.js */
 
-// const game = new Game();
-// game.startGame();
-// console.log(`Active Phrase - phrase: ${game.activePhrase.phrase}`);
 let game;
 
+/**
+ * Starts a new game on click of the 'Start Game' button
+ */
 document.getElementById('btn__reset').addEventListener('click', () => {
     game = new Game();
     game.startGame();
 });
 
+/**
+ * Makes '.key' buttons interactive on click
+ */
 document.getElementById('qwerty').addEventListener('click', event => {
     if (event.target.tagName === 'BUTTON') {
         game.handleInteraction(event.target);
     }
+});
+
+/**
+ * Allows for the interactivity of clicking '.key' buttons onscreen to be mirrored with the user keyboard
+ */
+document.addEventListener('keydown', event => {
+    document.querySelectorAll(`.key`).forEach(element => {
+        if (element.innerHTML === event.key) {
+            if ( !element.classList.contains('wrong') && !element.classList.contains('chosen') ) {
+                game.handleInteraction(element);
+            }
+        }
+    });
 });
